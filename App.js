811c29e7aea8695b2796558ffbd13b9b5366f488
Sabbir Hasan <sabbirhasan2999@gmail.com>
2020-12-09 19:56:12 +0600

    did some change on app.js

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import logo from '/assets/logo.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={logo} style={{ width: 305, height: 159 }} />
+      <Text style={{color:"green",fontSize:20}}>Open up App.js to start working on your app!</Text>
+      <Text style={{color:"orange",fontSize:80}}>Hi There! </Text>
       <StatusBar style="auto" />
     </View>
   );
